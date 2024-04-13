53da1fec5077c19d220fd103a9dc0b18fdadafba
isayan <raise.isayan@gmail.com>
2024-04-13 12:58:12 +0900

    コマンドライン対応

--- Readme.md
+++ Readme.md
@@ -35,6 +35,49 @@ A tab is added if the response is in image format and contains meta information.
 
 ![MetaData Tab](/image/MetaDataTab.png)
 
+## Command line option
+
+ It is possible to output Image metadata from the command line.
+
+```
+java -jar ImageMetaExtract.jar -file=<ImageFileName>
+```
+
+Specify the image for which you want to output <ImageFileName> metadata.
+The response will be output in tab-delimited format.
+
+ex)
+```
+java -jar ImageMetaExtract.jar -file=test.jpg
+
+JPEG    Compression Type        0xfffffffd      Baseline
+JPEG    Data Precision  0x0000  8 bits
+JPEG    Image Height    0x0001  1111 pixels
+JPEG    Image Width     0x0003  833 pixels
+JPEG    Number of Components    0x0005  3
+JPEG    Component 1     0x0006  Y component: Quantization table 0, Sampling factors 2 horiz/2 vert
+JPEG    Component 2     0x0007  Cb component: Quantization table 1, Sampling factors 1 horiz/1 vert
+JPEG    Component 3     0x0008  Cr component: Quantization table 1, Sampling factors 1 horiz/1 vert
+JFIF    Version 0x0005  1.1
+JFIF    Resolution Units        0x0007  none
+JFIF    X Resolution    0x0008  1 dot
+JFIF    Y Resolution    0x000a  1 dot
+JFIF    Thumbnail Width Pixels  0x000c  0
+JFIF    Thumbnail Height Pixels 0x000d  0
+Exif IFD0       Software        0x0131  Google
+ICC Profile     Profile Size    0x0000  3048
+ICC Profile     Version 0x0008  2.0.0
+ICC Profile     Class   0x000c  Display Device
+ICC Profile     Color space     0x0010  RGB
+ICC Profile     Profile Connection Space        0x0014  XYZ
+ICC Profile     Profile Date/Time       0x0018  2009:03:27 21:36:31
+ICC Profile     Signature       0x0024  acsp
+ICC Profile     Device attributes       0x0038  4294967296
+ICC Profile     XYZ values      0x0044  0.964 1 0.825
+ICC Profile     Tag Count       0x0080  16
+(...)
+```
+
 ## build
 
 ```
--- src/main/java/metadata/ImageMetaExtract.java
+++ src/main/java/metadata/ImageMetaExtract.java
@@ -3,13 +3,19 @@ package metadata;
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.imaging.ImageProcessingException;
 import com.drew.metadata.Metadata;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ImageMetaExtract {
 
     private final static Logger logger = Logger.getLogger(ImageMetaExtract.class.getName());
+
+    private final static java.util.ResourceBundle RELEASE = java.util.ResourceBundle.getBundle("burp/resources/release");
 
     public ImageMetaExtract() {
     }
@@ -24,6 +30,10 @@ public class ImageMetaExtract {
         }
     }
 
+    private static String getVersion() {
+        return RELEASE.getString("version");
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -31,7 +41,76 @@ public class ImageMetaExtract {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        String targetFileName = null;
+        boolean debug = false;
+        try {
+            for (String arg : args) {
+                // single parameter
+                if ("-v".equals(arg)) {
+                    System.out.println("Version: " + getVersion());
+                    System.out.println("Language: " + Locale.getDefault().getLanguage());
+                    System.exit(0);
+                }
+                if ("-h".equals(arg)) {
+                    usage();
+                    System.exit(0);
+                }
+                String[] param = arg.split("=", 2);
+                if (param.length < 2) {
+                    // single parameter
+                } else {
+                    // multi parameter
+                    if ("-file".equals(param[0])) {
+                        targetFileName = param[1];
+                    }
+                }
+            }
+
+            // 必須チェック
+            if (targetFileName == null) {
+                System.out.println("-file argument err ");
+                usage();
+                return;
+            } else {
+                File file = new File(targetFileName);
+                if (!file.exists()) {
+                    System.out.println("file not found:" + file.getAbsolutePath());
+                    usage();
+                    return;
+                }
+                ImageMetaExtract imageMeta = new ImageMetaExtract();
+                ImageMetaData meta = imageMeta.getMetaData(new FileInputStream(file));
+                System.out.println("Category\tTagName\tTagType\tDescription");
+                for (ImageRowData view : meta.getMetaData()) {
+                    System.out.print(view.getTag().getDirectoryName());
+                    System.out.print("\t");
+                    System.out.print(view.getTag().getTagName());
+                    System.out.print("\t");
+                    System.out.print(view.getTag().getTagTypeHex());
+                    System.out.print("\t");
+                    System.out.print(view.getTag().getDescription());
+                    System.out.println();
+                }
+                return;
+            }
+
+        } catch (Exception ex) {
+            String errmsg = String.format("%s: %s", ex.getClass().getName(), ex.getMessage());
+            System.out.println(errmsg);
+            logger.log(Level.SEVERE, ex.getMessage(), ex);
+            usage();
+        }
+    }
+
+    private static void usage() {
+        System.out.println("");
+        System.out.println(String.format("Usage: java -jar %s.jar [option] -file=ImagePath", ImageMetaExtract.class.getSimpleName()));
+        System.out.println("[option]");
+        System.out.println("\t-h: output help.");
+        System.out.println("\t-v: output version.");
+        System.out.println("[command]");
+        System.out.println("\t-file=<Image File>: Image File Path");
+        System.out.println("");
     }
 
 }
